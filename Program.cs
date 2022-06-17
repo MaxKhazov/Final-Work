b5eb63757221f025591e8536915e942697b651e9
MaxKh <mkhrussia@gmail.com>
2022-06-17 18:19:05 +0300

    Create new method for count symbols and create new array

--- Program.cs
+++ Program.cs
@@ -10,7 +10,21 @@ string? Element3 = Console.ReadLine();
 Console.WriteLine("Элемент массива 4/4:");
 string? Element4 = Console.ReadLine();
 
-string?[] StringArray = new string?[] {Element1, Element2, Element3, Element4};
+string?[] StringArray = new string?[] { Element1, Element2, Element3, Element4 };
 
 
 
+void CreateNewArray(string?[] arr)
+{
+    string?[] NewArray = new string?[4];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        int Length = arr[i].Length;
+        if (Length <= 3)
+        {
+            NewArray[i] = arr[i];
+        }
+        Console.Write($"({NewArray[i]}) ");
+    }
+
+}
